e71225bbee968c3fe27b8f389d7fc62553e04af7
Beatriz <beatrizgomez92@gmail.com>
2022-10-25 02:10:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <header className='header'>header</header>
+      <main>main</main>
+      <footer>footer</footer>
+    </div>
+  );
+};
+
+export default App;
