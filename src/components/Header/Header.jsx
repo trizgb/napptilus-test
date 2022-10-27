d00e0326e17edb2008b3d3f1b05d5629676a1412
Beatriz <beatrizgomez92@gmail.com>
2022-10-27 17:59:17 +0200

    Breadcrumb

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
 import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
 import Cookies from 'js-cookie';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
   const { count } = useContext(CartContext);
   const cookie = Cookies.get('cart_count');
+  const { pathname } = useLocation();
+
+  const paths = pathname.split('/').slice(1);
+
+  const breadcrumb = (
+    <nav className='flex space-x-2 text-xs'>
+      <Link to='/'>
+        <span className='hover:underline'>Home</span>
+      </Link>
+      <span>/</span>
+      {paths.length > 1 ? (
+        <Link to={pathname}>
+          <span className='hover:underline'>{paths[1]}</span>
+        </Link>
+      ) : null}
+    </nav>
+  );
 
   return (
     <header className='w-full p-4 bg-white'>
@@ -14,7 +32,7 @@ const Header = () => {
             <h1 className='font-logo text-lg lg:text-3xl'>MobVice</h1>
           </a>
         </div>
-        <div>Breadcrumbs</div>
+        <div>{breadcrumb}</div>
         <div className='relative flex'>
           <span className='material-symbols-outlined text-4xl'>
             shopping_bag
